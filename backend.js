e468086f292bfb2ae66548319b69a507d006f8da
AmberDenblinden <amber.denblinden@student.ehb.be>
2025-11-28 10:27:32 +0100

    updated to root folder

---
+++ backend.js
@@ -0,0 +1,36 @@
+import express from "express";
+import { readFile, writeFile } from "node:fs/promises";
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(express.json()); //alle data van en naar de api is uniek
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
+
+app.use(express.static("public"));
+
+app.get("/api/carnaval-groups", async (req, res) => {
+	const contents = await readFile("comments.json", { encoding: "utf8" });
+	const data = JSON.parse(contents);
+	res.json(data);
+});
+app.get("/api/carnaval-goups", async (req, res) => {
+	//To get individually each id of the comments
+	let id = req.query.id;
+	//Read the comments file
+	const contents = await readFile("comments.json", { encoding: "utf8" });
+	const data = JSON.parse(contents);
+	//get one bg
+	let coms = data[id]; //vb. data.120677
+	res.json(coms);
+});
+
+app.post("/api/carnaval-groups", async (req, res) => {
+	//Request the body so you can post a new comment via a body
+	let data = req.body;
+	console.log(data);
+	res.send("succces");
+});
